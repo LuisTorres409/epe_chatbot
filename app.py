61468c770fccc3e38fc5539a1c15c39ce23ca0ed
LuisTorres409 <123283593+LuisTorres409@users.noreply.github.com>
2024-03-07 15:03:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,9 @@ if st.session_state.messages[-1]["role"] != "assistant":
                     st.write(response_message,unsafe_allow_html=True)
                     #st.write(doc_preview, unsafe_allow_html=True)
                     doc_link = f'https://epe-pdfs.s3.sa-east-1.amazonaws.com/{nome_arquivo.replace(" ","+")}'
-                    st.link_button('Download',doc_link)
+                    col1 , col2 = st.columns(2)
+                    col.link_button('Download',doc_link)
+                    col2.markdown(f'**{nome_arquivo}**')
                     #pdf_viewer(input = file_bytes,pages_to_render=1)
                     st.session_state.messages.append({"role": "assistant", "content": response_message, "file": nome_arquivo , "has_file": True})
                 except:
