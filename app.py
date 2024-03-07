7e5de42da66ef619637842cfff17e11d411e8635
LuisTorres409 <123283593+LuisTorres409@users.noreply.github.com>
2024-03-07 12:12:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
                     response_message = (response.response + f'\n\n Aqui está o documento relacionado a sua pergunta: ')
                     st.write(response_message,unsafe_allow_html=True)
                     #st.write(doc_preview, unsafe_allow_html=True)
-                    pdf_viewer(input = file_bytes,width=700)
+                    pdf_viewer(input = file_bytes,width=200,pages_to_render=1,height = 300)
                     st.session_state.messages.append({"role": "assistant", "content": response_message, "file": doc_preview , "has_file": True})
                 except:
                     response_message = response.response
